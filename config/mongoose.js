3239d9132286db495cc492dfa6cae5ed511fa74f
Chirag <chiragbansal184@gmail.com>
2022-03-17 15:10:54 +0530

    Added mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 const env=require('..//config/environment');
-mongoose.connect(`mongodb://mongodb+srv://chiragb184:9pRkceZH9h1Mi9xScluster0.lne9m.mongodb.net/${env.db}`);
+mongoose.connect(`mongodb+srv://chiragb184:bansal184@cluster0.lne9m.mongodb.net/${env.db}`);
 
 //creating connection with database
 const db = mongoose.connection;
